c5e9816fddea2b446bba2cf0c16ae5ec36dbe7f7
ComdotKkhHimel <kkhappcreators@gmail.com>
2025-04-28 22:01:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let maleNames = [];
+let femaleNames = [];
+
+async function loadNames() {
+  const maleData = await fetch('male_names.txt').then(res => res.text());
+  const femaleData = await fetch('female_names.txt').then(res => res.text());
+  maleNames = maleData.split('\n').filter(Boolean);
+  femaleNames = femaleData.split('\n').filter(Boolean);
+}
+
+loadNames();
+
+document.getElementById('generateBtn').addEventListener('click', () => {
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const namesList = gender === 'male' ? maleNames : femaleNames;
+  const randomName = namesList[Math.floor(Math.random() * namesList.length)];
+  document.getElementById('nameResult').textContent = randomName;
+});
